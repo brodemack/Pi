8667d027fc92e9379ff6a2651bca2785703439b8
Nick Otter <otternq@gmail.com>
2014-07-19 10:46:43 -0600

    Update motion-setup.sh

--- motion-setup.sh
+++ motion-setup.sh
@@ -3,4 +3,5 @@ sudo apt-get install -y libjpeg62
 
 mkdir -p ~/mmal
 
-(cd ~/mmal/; wget https://www.dropbox.com/s/xdfcxm5hu71s97d/motion-mmal.tar.gz && tar -zxvf motion-mmal.tar.gz)
+(cd ~/mmal/; wget https://www.dropbox.com/s/xdfcxm5hu71s97d/motion-mmal.tar.gz)
+(cd ~/mmal/; tar -zxvf motion-mmal.tar.gz)
