1cc94120786bea978830740456a38ec88671d882
otternq <otternq@gmail.com>
2014-07-19 10:44:08 -0600

    chainging tar after wget

--- motion-setup.sh
+++ motion-setup.sh
@@ -3,6 +3,4 @@ sudo apt-get install -y libjpeg62
 
 mkdir -p ~/mmal
 
-(cd ~/mmal/; wget https://www.dropbox.com/s/xdfcxm5hu71s97d/motion-mmal.tar.gz)
-
-tar -zxvf ~/mmal/motion-mmal.tar.gz ~/mmal/motion-mmal
+(cd ~/mmal/; wget https://www.dropbox.com/s/xdfcxm5hu71s97d/motion-mmal.tar.gz && tar -zxvf motion-mmal.tar.gz)
