f592114d4c4061d22dcb2ff4344cb023c59cc41f
otternq <otternq@gmail.com>
2014-07-19 11:08:03 -0600

    ruby download and update conf

--- motion-setup.sh
+++ motion-setup.sh
@@ -5,3 +5,4 @@ mkdir -p ~/mmal
 
 (cd ~/mmal/; wget https://www.dropbox.com/s/xdfcxm5hu71s97d/motion-mmal.tar.gz)
 (cd ~/mmal/; tar -zxvf motion-mmal.tar.gz)
+(cd ~/mmal/; wget https://raw.githubusercontent.com/brodemack/Pi/master/update-motion-conf.rb; ruby update-motion-conf.rb < motion-mmalcam.conf > motion-mmalcam.conf)
