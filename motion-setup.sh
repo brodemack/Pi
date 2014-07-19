fc623ce12eb4a5881da5533b753942d637f49c6a
otternq <otternq@gmail.com>
2014-07-19 10:16:22 -0600

    Starting to script the setup process

---
+++ motion-setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get install motion
+sudo apt-get install libjpeg62
+
+mkdir -p ~/mmal
+
+wget https://www.dropbox.com/s/xdfcxm5hu71s97d/motion-mmal.tar.gz ~/mmal
+
+tar -zxvf ~/mmal/motion-mmal.tar.gz ~/mmal/motion-mmal
---
+++ ri-setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade -y #needs to force
+sudo apt-get install rpi-update
+sudo rpi-update
+
+echo "now restart with sudo shutdown -r now"
